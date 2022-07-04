452605f335d29ddcd6bddb8aaf37dd3dac9e4e65
DEV\DEVOPS <eksaydam@gmail.com>
2022-07-04 23:32:50 +0300

    site_bot güncelleme

--- site_bot/BeautifulSoup_site_bot.py
+++ site_bot/BeautifulSoup_site_bot.py
@@ -3,10 +3,9 @@ import requests
 from bs4 import BeautifulSoup  # pip install beautifulsoup4
 # https://www.crummy.com/software/BeautifulSoup/bs4/doc/
 import pandas as pd
-import csv
 
 # url = 'https://www.tripadvisor.in/Hotels-g28932-Hawaii-Hotels.html'
-url = 'https://www.tripadvisor.com.tr/Hotels-g293969-Turkey-Hotels.html'
+URL = 'https://www.tripadvisor.com.tr/Hotels-g293969-Turkey-Hotels.html'
 user_agent = ({'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
                               AppleWebKit/537.36 (KHTML, like Gecko) \
                               Chrome/90.0.4430.212 Safari/537.36',
@@ -24,7 +23,7 @@ def sayfa_icerigini_al(url_bilgisi):
     return BeautifulSoup(page.text, features='html.parser')
 
 
-soup = sayfa_icerigini_al(url)
+soup = sayfa_icerigini_al(URL)
 for otel_ismi in soup.findAll('div', {'class': 'listing_title'}):
     otel_isimleri.append(" ".join(str(otel_ismi.text).split()[1:]))
 
@@ -39,7 +38,9 @@ for yorum in soup.findAll('a', {'class', 'review_count'}):
 fiyatlar = []
 for fiyat in soup.findAll('div', {'class', 'price-wrap'}):
     # print(str(fiyat.text))
-    fiyatlar.append(str(fiyat.text).rsplit('₺')[-1]+'₺')
+    # fiyatlar.append(str(fiyat.text).rsplit('₺')[-1]+'₺')
+    # print(str(fiyat.text).rsplit('₺'))
+    fiyatlar.append(str(fiyat.text).rsplit('₺',maxsplit=1)[-1]+'₺')
 
 veri = {'OTEL ISIMLERI': otel_isimleri,
         'DERECELENDIRME': derecelendirme,
@@ -48,6 +49,6 @@ veri = {'OTEL ISIMLERI': otel_isimleri,
 
 # dataframe oluşturma.
 veri_ambari = pd.DataFrame.from_dict(veri)
-print(veri_ambari.head(10))
+print(veri_ambari.head(30))
 
 veri_ambari.to_csv('oteller.csv', index=False, header=True)
