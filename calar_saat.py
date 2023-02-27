291fe3a28f48d20a1f1619cb820cba40290f6547
DEV\DEVOPS <eksaydam@gmail.com>
2023-02-27 23:22:41 +0300

    calar saat fix

--- calar_saat.py
+++ calar_saat.py
@@ -1,33 +1,37 @@
 """Çalar saat."""
 from datetime import datetime
+
 # from tracemalloc import start
 from playsound import playsound
+
 # from os import startfile
 # from pydub import AudioSegment,playback
-alarm_zaman = input("Alarm zamanını giriniz [SS:DD:ss]: ")
-alarm_saat = alarm_zaman.split(':')[0]
-alarm_dakika = alarm_zaman.split(':')[1]
-alarm_saniye = alarm_zaman.split(':')[2]
+alarm_zaman = input("Alarm zamanini giriniz [SS:DD:ss]: ")
+alarm_saat = alarm_zaman.split(":")[0]
+alarm_dakika = alarm_zaman.split(":")[1]
+alarm_saniye = alarm_zaman.split(":")[2]
 print("Alarm ayarlandi.")
-GOSTER = 1
+goster = 1
 while True:
     suan = datetime.now()
-    suan_saat = suan.strftime('%I')
-    suan_dakika = suan.strftime('%M')
-    suan_saniye = suan.strftime('%S')
-    if GOSTER == 1:
+    suan_saat = suan.strftime("%I")
+    suan_dakika = suan.strftime("%M")
+    suan_saniye = suan.strftime("%S")
+    if goster == 1:
         # print(suan)
         # print(suan_saat, suan_dakika, suan_saniye)
         # print(alarm_saat, alarm_dakika, alarm_saniye)
-        GOSTER -= 1
+        goster -= goster
 
-    if (alarm_saat == suan_saat and
-        alarm_dakika == suan_dakika and
-            alarm_saniye <= suan_saniye):
-        print("ALARM çalınıyor.")
-        playsound('alarm2.mp3')
+    if (
+        alarm_saat == suan_saat
+        and alarm_dakika == suan_dakika
+        and alarm_saniye <= suan_saniye
+    ):
+        print("ALARM çaliniyor.")
+        playsound("alarm2.mp3")
         # startfile('onnumara.mp3')
         # ses = AudioSegment.from_file("onnumara.mp4", "mp4")
         # playback.play(ses)
-        print("Alarm sonlandırıldı.")
+        print("Alarm sonlandirildi.")
         break
