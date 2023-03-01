50ed15c7ff0ed6a41a53e2cf1e969f3eb787758f
DEV\DEVOPS <eksaydam@gmail.com>
2023-03-01 22:20:18 +0300

    small change

--- cv2/hand_detector/hand_detector.py
+++ cv2/hand_detector/hand_detector.py
@@ -3,14 +3,14 @@ import cv2
 from cvzone.HandTrackingModule import HandDetector
 
 detector = HandDetector(detectionCon=0.8, maxHands=2)
-video = cv2.VideoCapture()
+video = cv2.VideoCapture(0)
 
 while True:
     ret, frame = video.read()
     hands, img = detector.findHands(frame)
     cv2.imshow("Frame", frame)
-    k=cv2.waitKey(1)
-    if k == ord('q'):
+    k = cv2.waitKey(1)
+    if k == ord("q"):
         break
 
 video.relase()
