4d99a8b0b8c1fcd5085b8e916c169a5c53a714ff
DEV\DEVOPS <eksaydam@gmail.com>
2023-03-01 22:06:44 +0300

    opencv-python example

---
+++ cv2/hand_detector/hand_detector.py
@@ -0,0 +1,17 @@
+"""Hand Detector."""
+import cv2
+from cvzone.HandTrackingModule import HandDetector
+
+detector = HandDetector(detectionCon=0.8, maxHands=2)
+video = cv2.VideoCapture()
+
+while True:
+    ret, frame = video.read()
+    hands, img = detector.findHands(frame)
+    cv2.imshow("Frame", frame)
+    k=cv2.waitKey(1)
+    if k == ord('q'):
+        break
+
+video.relase()
+cv2.destroyAllWindows()
